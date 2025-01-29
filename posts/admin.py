1e08ad0d5d65401e0ea8bb305a0302ebf971794e
xxdakexx28 <bajormi.2807@gmail.com>
2025-01-29 18:03:28 -0400

    app-model

---
+++ posts/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Post
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'date_posted', 'last_modified')
+    list_filter = ('author', 'date_posted', 'last_modified')
+    search_fields = ('title', 'content')
+    ordering = ('date_posted',)
+
+admin.site.register(Post, PostAdmin)
+
+
+
+    
