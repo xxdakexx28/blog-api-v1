a84ea697807a5fc9af949c358547b9dd5ad963a3
xxdakexx28 <bajormi.2807@gmail.com>
2025-02-03 11:38:54 -0400

    cambios

--- posts/admin.py
+++ posts/admin.py
@@ -5,9 +5,9 @@ from .models import Post
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'date_posted', 'last_modified')
-    list_filter = ('author', 'date_posted', 'last_modified')
+    list_filter = ('author', 'date_posted')
     search_fields = ('title', 'content')
-    ordering = ('date_posted',)
+    ordering = ['date_posted',]
 
 admin.site.register(Post, PostAdmin)
 
